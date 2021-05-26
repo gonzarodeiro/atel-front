f4aa11286e66158ce7285a479114f86712f84b7f
Gonzalo <chalorodeiro@gmail.com>
2021-05-26 14:56:52 -0300

    refactor Jitsi

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-
         <p>Pruebita commit</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
-      <div class="topcorner">
-        <Jitsi room="CODE-ROOM-TEST" userName="Augusto" />
+      <div className='topcorner'>
+        <Jitsi room='CODE-ROOM-TEST' userName='Augusto' />
       </div>
     </div>
   );
 }
-
 
 export default App;
