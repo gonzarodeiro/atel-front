bb023b3b8fd434d6626bc7dcc9c30887ab50ce4c
Augusto Uricchio <augustouricchio@gmail.com>
2021-05-26 00:22:10 -0300

    add configurable jitsi component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
+import Jitsi from './components/Jitsi.js';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+
         <p>Pruebita commit</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
+      <div class="topcorner">
+        <Jitsi room="CODE-ROOM-TEST" userName="Augusto" />
+      </div>
     </div>
   );
 }
+
 
 export default App;
