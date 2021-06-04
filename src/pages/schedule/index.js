dde03f159c0a4b562964304e45b8f7620c9da805
Gonzalo <chalorodeiro@gmail.com>
2021-06-03 23:46:11 -0300

    minor fix

--- src/pages/schedule/index.js
+++ src/pages/schedule/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Layout from '../../utils/layout/index';
-import Table from '../../components/table';
+import Table from '../../components/Table';
 import Loading from '../../components/Loading';
 import module from '../../utils/enums/modules';
 import Footer from '../../components/html/Footer';
