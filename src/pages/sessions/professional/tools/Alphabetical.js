43cf5698db3296744ca58ff1f3c5deb29e5f8411
Gonzalo <chalorodeiro@gmail.com>
2021-06-15 23:23:26 -0300

    install konva

--- src/pages/sessions/professional/tools/Alphabetical.js
+++ src/pages/sessions/professional/tools/Alphabetical.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MDBBtn } from 'mdbreact';
 import Jitsi from '../../../../components/Jitsi';
 import swal from '@sweetalert/with-react';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
 const Alphabetical = ({ props, handleChange, session, showTools, showMeeting }) => {
   function redirectTool(tool) {
@@ -36,7 +37,17 @@ const Alphabetical = ({ props, handleChange, session, showTools, showMeeting }) 
   return (
     <React.Fragment>
       <div className='row'>
-        <div className='pb-3 mt-2 col-md-8'>{props.location.state && <Jitsi roomId={props.location.state.roomId + '-' + props.location.state.sessionId} userName={sessionStorage.getItem('name')} />}</div>
+        <div className='pb-3 mt-2 col-md-8'>
+          <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+              <Text text='Some text on canvas' fontSize={15} />
+              <Rect x={20} y={50} width={100} height={100} fill='red' shadowBlur={10} />
+              <Circle x={200} y={100} radius={50} fill='green' />
+              <Line x={20} y={200} points={[0, 0, 100, 0, 100, 100]} tension={0.5} closed stroke='black' fillLinearGradientStartPoint={{ x: -50, y: -50 }} fillLinearGradientEndPoint={{ x: 50, y: 50 }} fillLinearGradientColorStops={[0, 'red', 1, 'yellow']} />
+            </Layer>
+          </Stage>
+          {/* {props.location.state && <Jitsi roomId={props.location.state.roomId + '-' + props.location.state.sessionId} userName={sessionStorage.getItem('name')} />} */}
+        </div>
         <div className='col-md-4' style={{ marginTop: '3px' }}>
           <div data-test='col'>
             <label className='mb-2' style={{ fontSize: '13px', fontWeight: 'bold' }}>
