f22438db105428b107e017deb26ad99613d689d1
Augusto Uricchio <augustouricchio@gmail.com>
2021-06-26 01:12:04 -0300

    wip

--- src/components/Activity/Alphabetical/index.js
+++ src/components/Activity/Alphabetical/index.js
@@ -1,10 +1,23 @@
-import React, { useRef, useState, useEffect, useCallback } from 'react';
-import { Stage, Layer, Image as KonvaImage, Text, Group, Arrow, Circle } from 'react-konva';
-import { startAnimationConfites, generateConfites, getRandomItems } from './confites';
-import banner from './images/others/banner.png';
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import {
+  Stage,
+  Layer,
+  Image as KonvaImage,
+  Text,
+  Group,
+  Arrow,
+  Circle,
+} from "react-konva";
+import {
+  startAnimationConfites,
+  generateConfites,
+  getRandomItems,
+} from "./confites";
+import banner from "./images/others/banner.png";
+import correctBanner from "./images/others/correctBanner.png";
 
 const Alphabetical = ({ data }) => {
-  const CONTAINER_SIZE = '100%',
+  const CONTAINER_SIZE = "100%",
     MARGIN = 20,
     MARGIN_TOP = 20,
     BANNER_SIZE = 250,
@@ -20,7 +33,9 @@ const Alphabetical = ({ data }) => {
   const confites = generateConfites(100, divRef);
   const elementsToUse = getRandomItems(data.elements);
   const [itemGroupLeft, setItemGroupLeft] = useState(elementsToUse);
-  const [itemGroupRight, setItemGroupRight] = useState(getRandomItems(elementsToUse));
+  const [itemGroupRight, setItemGroupRight] = useState(
+    getRandomItems(elementsToUse)
+  );
   const [color, setColor] = useState(getRandomItems(data.colors)[0]);
   const [originPoint, setOriginPoint] = useState([]);
   const [targetPoint, setTargetPoint] = useState([]);
@@ -62,22 +77,31 @@ const Alphabetical = ({ data }) => {
   function onTargetMouseUp(element) {
     if (itemLeftSelected === element.name) {
       setShowConfites(true);
+      let banner = layerRef.current.find("#banner" + element.id);
+      for (var i in itemGroupLeft) {
+        if (itemGroupLeft[i].name == itemLeftSelected) {
+          itemGroupLeft[i].matched = true;
+          break;
+        }
+      }
+      banner[0].image(imageFactory(correctBanner));
+      banner[0].draw();
       startAnimationConfites(stageRef, layerRef);
     }
   }
 
   function imageFactory(x) {
-    const rv = document.createElement('img');
+    const rv = document.createElement("img");
     rv.src = x;
     return rv;
   }
 
   function oposedColor(color) {
     const aux = color.substring(1);
-    const hex = '0x' + aux;
+    const hex = "0x" + aux;
     const num = parseInt(hex);
-    const comp = parseInt('0xffffff') - num;
-    return '#' + comp.toString(16) + 'ff';
+    const comp = parseInt("0xffffff") - num;
+    return "#" + comp.toString(16) + "ff";
   }
 
   function handleLeftItem(element) {
@@ -90,25 +114,99 @@ const Alphabetical = ({ data }) => {
   }
 
   return (
-    <div style={{ width: CONTAINER_SIZE, height: CONTAINER_SIZE, backgroundColor: color }} ref={divRef}>
-      <Stage width={width} height={height} d ref={stageRef} onMouseMove={onMouseMove}>
+    <div
+      style={{
+        width: CONTAINER_SIZE,
+        height: CONTAINER_SIZE,
+        backgroundColor: color,
+      }}
+      ref={divRef}
+    >
+      <Stage
+        width={width}
+        height={height}
+        ref={stageRef}
+        onMouseMove={onMouseMove}
+      >
         <Layer ref={layerRef}>
           {itemGroupLeft &&
             itemGroupLeft.map((element, index) => (
               <Group onMouseUp={() => handleLeftItem(element)}>
-                <KonvaImage key={element.id} id={element.id} x={MARGIN} y={MARGIN_TOP + (MARGIN + element.height) * index} width={element.width} height={element.height} image={imageFactory(element.src)} />
+                <KonvaImage
+                  key={element.id}
+                  id={element.id}
+                  x={MARGIN}
+                  y={MARGIN_TOP + (MARGIN + element.height) * index}
+                  width={element.width}
+                  height={element.height}
+                  image={imageFactory(element.src)}
+                />
               </Group>
             ))}
           {itemGroupRight &&
             itemGroupRight.map((element, index) => (
-              <Group key={element.id} id={element.id} x={width - BANNER_SIZE - MARGIN} y={MARGIN_TOP + (MARGIN + element.height) * index} onMouseUp={() => onTargetMouseUp(element)}>
-                <KonvaImage image={imageFactory(banner)} height={BANNER_SIZE * BANNER_RATIO} width={BANNER_SIZE} />
-                <Text text={element.name} height={element.height} width={BANNER_SIZE} fontVariant='bold' fontSize={24} align='center' verticalAlign='middle' strokeWidth={1} fill='white' shadowColor='black' shadowBlur={10} />
+              <Group
+                key={element.id}
+                id={element.id}
+                x={width - BANNER_SIZE - MARGIN}
+                y={MARGIN_TOP + (MARGIN + element.height) * index}
+                onMouseUp={() => onTargetMouseUp(element)}
+              >
+                <KonvaImage
+                  id={"banner" + element.id}
+                  image={
+                    element.matched == false
+                      ? imageFactory(banner)
+                      : imageFactory(correctBanner)
+                  }
+                  height={BANNER_SIZE * BANNER_RATIO}
+                  width={BANNER_SIZE}
+                />
+                <Text
+                  text={element.name}
+                  height={element.height}
+                  width={BANNER_SIZE}
+                  fontVariant="bold"
+                  fontSize={24}
+                  align="center"
+                  verticalAlign="middle"
+                  strokeWidth={1}
+                  fill="white"
+                  shadowColor="black"
+                  shadowBlur={10}
+                />
               </Group>
             ))}
-          <Arrow ref={arrowRef} points={arrowPoints.length === 4 ? arrowPoints : originPoint.concat(targetPoint)} fill={oposedColor(color)} stroke={oposedColor(color)} strokeWidth={8} onClick={onTargetClick} lineJoin='round' lineCap='round' visible={arrowPoints.length || targetPoint.length} />
+          <Arrow
+            ref={arrowRef}
+            points={
+              arrowPoints.length === 4
+                ? arrowPoints
+                : originPoint.concat(targetPoint)
+            }
+            fill={oposedColor(color)}
+            stroke={oposedColor(color)}
+            strokeWidth={8}
+            onClick={onTargetClick}
+            lineJoin="round"
+            lineCap="round"
+            visible={arrowPoints.length || targetPoint.length}
+          />
         </Layer>
-        {showConfites && <Layer>{confites && confites.map((element) => <Circle id={'circle'} x={element.confite.x} y={element.confite.y} radius={element.confite.radius} fill={element.confite.fill} />)}</Layer>}
+        {showConfites && (
+          <Layer>
+            {confites &&
+              confites.map((element) => (
+                <Circle
+                  id={"circle"}
+                  x={element.confite.x}
+                  y={element.confite.y}
+                  radius={element.confite.radius}
+                  fill={element.confite.fill}
+                />
+              ))}
+          </Layer>
+        )}
       </Stage>
     </div>
   );
--- src/pages/sessions/professional/tools/Alphabetical.js
+++ src/pages/sessions/professional/tools/Alphabetical.js
@@ -1,65 +1,76 @@
-import React, { useState } from 'react';
-import { MDBBtn } from 'mdbreact';
-import Jitsi from '../../../../components/Jitsi';
-import Activity from '../../../../components/Activity/Alphabetical/index';
-import finishSession from '../finishSession';
-import { v4 as uuidv4 } from 'uuid';
-import imgCherry from '../../../../components/Activity/Alphabetical/images/fruits/fruit_cherry.png';
-import imgGrape from '../../../../components/Activity/Alphabetical/images/fruits/fruit_grape.png';
-import imgApple from '../../../../components/Activity/Alphabetical/images/fruits/fruit_apple.png';
-import imgLemon from '../../../../components/Activity/Alphabetical/images/fruits/fruit_lemon.png';
-import imgBanana from '../../../../components/Activity/Alphabetical/images/fruits/fruit_banana.png';
-import imgCat from '../../../../components/Activity/Alphabetical/images/animals/cat.png';
-import imgGiraffe from '../../../../components/Activity/Alphabetical/images/animals/giraffe.png';
-import imgLion from '../../../../components/Activity/Alphabetical/images/animals/lion.png';
-import imgDog from '../../../../components/Activity/Alphabetical/images/animals/dog.png';
-import imgElephant from '../../../../components/Activity/Alphabetical/images/animals/elephant.png';
+import React, { useState } from "react";
+import { MDBBtn } from "mdbreact";
+import Jitsi from "../../../../components/Jitsi";
+import Activity from "../../../../components/Activity/Alphabetical/index";
+import finishSession from "../finishSession";
+import { v4 as uuidv4 } from "uuid";
+import imgCherry from "../../../../components/Activity/Alphabetical/images/fruits/fruit_cherry.png";
+import imgGrape from "../../../../components/Activity/Alphabetical/images/fruits/fruit_grape.png";
+import imgApple from "../../../../components/Activity/Alphabetical/images/fruits/fruit_apple.png";
+import imgLemon from "../../../../components/Activity/Alphabetical/images/fruits/fruit_lemon.png";
+import imgBanana from "../../../../components/Activity/Alphabetical/images/fruits/fruit_banana.png";
+import imgCat from "../../../../components/Activity/Alphabetical/images/animals/cat.png";
+import imgGiraffe from "../../../../components/Activity/Alphabetical/images/animals/giraffe.png";
+import imgLion from "../../../../components/Activity/Alphabetical/images/animals/lion.png";
+import imgDog from "../../../../components/Activity/Alphabetical/images/animals/dog.png";
+import imgElephant from "../../../../components/Activity/Alphabetical/images/animals/elephant.png";
 
-const Alphabetical = ({ props, handleChange, session, showTools, showMeeting }) => {
+const Alphabetical = ({
+  props,
+  handleChange,
+  session,
+  showTools,
+  showMeeting,
+}) => {
   const [activityData, setActivityData] = useState({
     elements: [
       {
         id: uuidv4(),
-        name: 'CEREZA',
+        name: "CEREZA",
         src: imgCherry,
         width: 90,
         height: 85,
-        draggable: true
+        draggable: true,
+        matched: false,
       },
       {
         id: uuidv4(),
-        name: 'UVA',
+        name: "UVA",
         src: imgGrape,
         width: 90,
         height: 85,
-        draggable: true
+        draggable: true,
+        matched: false,
       },
       {
         id: uuidv4(),
-        name: 'MANZANA',
+        name: "MANZANA",
         src: imgApple,
         width: 90,
         height: 85,
-        draggable: true
+        draggable: true,
+        matched: false,
       },
       {
         id: uuidv4(),
-        name: 'LIMÓN',
+        name: "LIMÓN",
         src: imgLemon,
         width: 90,
         height: 85,
-        draggable: true
+        draggable: true,
+        matched: false,
       },
       {
         id: uuidv4(),
-        name: 'BANANA',
+        name: "BANANA",
         src: imgBanana,
         width: 90,
         height: 85,
-        draggable: true
-      }
+        draggable: true,
+        matched: false,
+      },
     ],
-    colors: ['#DE8971', '#7B6079', '#A7D0CD', '#FFE9D6']
+    colors: ["#DE8971", "#7B6079", "#A7D0CD", "#FFE9D6"],
   });
 
   function redirectTool(tool) {
@@ -76,104 +87,152 @@ const Alphabetical = ({ props, handleChange, session, showTools, showMeeting }) 
       elements: [
         {
           id: uuidv4(),
-          name: 'GATO',
+          name: "GATO",
           src: imgCat,
           width: 90,
           height: 85,
-          draggable: true
+          draggable: true,
         },
         {
           id: uuidv4(),
-          name: 'JIRAFA',
+          name: "JIRAFA",
           src: imgGiraffe,
           width: 90,
           height: 85,
-          draggable: true
+          draggable: true,
         },
         {
           id: uuidv4(),
-          name: 'LEÓN',
+          name: "LEÓN",
           src: imgLion,
           width: 90,
           height: 85,
-          draggable: true
+          draggable: true,
         },
         {
           id: uuidv4(),
-          name: 'PERRO',
+          name: "PERRO",
           src: imgDog,
           width: 90,
           height: 85,
-          draggable: true
+          draggable: true,
         },
         {
           id: uuidv4(),
-          name: 'ELEFANTE',
+          name: "ELEFANTE",
           src: imgElephant,
           width: 90,
           height: 85,
-          draggable: true
-        }
+          draggable: true,
+        },
       ],
-      colors: ['#DE8971', '#7B6079', '#A7D0CD', '#FFE9D6']
+      colors: ["#DE8971", "#7B6079", "#A7D0CD", "#FFE9D6"],
     });
   }
 
   return (
     <React.Fragment>
-      <div className='row'>
-        <div className='pb-3 mt-2 col-md-8'>
+      <div className="row">
+        <div className="pb-3 mt-2 col-md-8">
           <Activity data={activityData} />
         </div>
-        <div className='col-md-4' style={{ marginTop: '3px' }}>
-          <div data-test='col'>
-            <label className='mb-1' style={{ fontSize: '13px', fontWeight: 'bold' }}>
+        <div className="col-md-4" style={{ marginTop: "3px" }}>
+          <div data-test="col">
+            <label
+              className="mb-1"
+              style={{ fontSize: "13px", fontWeight: "bold" }}
+            >
               Cámara del alumno
             </label>
           </div>
-          {props.location.state && <Jitsi roomId={props.location.state.roomId + '-' + props.location.state.sessionId} userName={sessionStorage.getItem('name')} height='200px' />}
-          <div data-test='col' style={{ paddingTop: '12px' }}>
-            <label className='mb-1' style={{ fontSize: '13px', fontWeight: 'bold' }}>
+          {props.location.state && (
+            <Jitsi
+              roomId={
+                props.location.state.roomId +
+                "-" +
+                props.location.state.sessionId
+              }
+              userName={sessionStorage.getItem("name")}
+              height="200px"
+            />
+          )}
+          <div data-test="col" style={{ paddingTop: "12px" }}>
+            <label
+              className="mb-1"
+              style={{ fontSize: "13px", fontWeight: "bold" }}
+            >
               Acciones
             </label>
           </div>
-          <div data-test='container' className='container-fluid section mb-3 border p-3 col-md-12'>
-            <div className='row'>
-              <div className='col-md-12'>
-                <MDBBtn onClick={restart} size='lg' className='py-2 green darken-2 shadow-none text-white btnOption w-100 ml-0'>
+          <div
+            data-test="container"
+            className="container-fluid section mb-3 border p-3 col-md-12"
+          >
+            <div className="row">
+              <div className="col-md-12">
+                <MDBBtn
+                  onClick={restart}
+                  size="lg"
+                  className="py-2 green darken-2 shadow-none text-white btnOption w-100 ml-0"
+                >
                   <span>Reiniciar actividad</span>
                 </MDBBtn>
               </div>
-              <div className='col-md-12 mt-2'>
-                <MDBBtn onClick={() => redirectTool('numerical')} size='lg' className='py-2 blue darken-2 shadow-none text-white btnOption w-100 ml-0'>
+              <div className="col-md-12 mt-2">
+                <MDBBtn
+                  onClick={() => redirectTool("numerical")}
+                  size="lg"
+                  className="py-2 blue darken-2 shadow-none text-white btnOption w-100 ml-0"
+                >
                   <span>Herramienta Númerica</span>
                 </MDBBtn>
               </div>
-              <div className='col-md-12 mt-2'>
-                <MDBBtn onClick={() => redirectTool('pictogram')} size='lg' className='py-2 blue darken-2 shadow-none text-white btnOption w-100 ml-0'>
+              <div className="col-md-12 mt-2">
+                <MDBBtn
+                  onClick={() => redirectTool("pictogram")}
+                  size="lg"
+                  className="py-2 blue darken-2 shadow-none text-white btnOption w-100 ml-0"
+                >
                   <span>Pictogramas</span>
                 </MDBBtn>
               </div>
             </div>
           </div>
-          <div data-test='col'>
-            <label className='mb-1' style={{ fontSize: '13px', fontWeight: 'bold' }}>
+          <div data-test="col">
+            <label
+              className="mb-1"
+              style={{ fontSize: "13px", fontWeight: "bold" }}
+            >
               Fin de la sesión
             </label>
           </div>
-          <div data-test='container' className='container-fluid section mb-3 border p-3 col-md-12'>
-            <div className='col-md-12 mb-1'>
-              <MDBBtn onClick={() => finishSession(redirectEnd)} size='lg' className='py-2 shadow-none btnOption btnCancel w-100 ml-0'>
+          <div
+            data-test="container"
+            className="container-fluid section mb-3 border p-3 col-md-12"
+          >
+            <div className="col-md-12 mb-1">
+              <MDBBtn
+                onClick={() => finishSession(redirectEnd)}
+                size="lg"
+                className="py-2 shadow-none btnOption btnCancel w-100 ml-0"
+              >
                 <span>Finalizar</span>
               </MDBBtn>
             </div>
           </div>
         </div>
       </div>
-      <div className='row mb-2'>
-        <div className='col-md-12 my-1'>
+      <div className="row mb-2">
+        <div className="col-md-12 my-1">
           <label>Observaciones de la actividad</label>
-          <textarea id='alphabeticalComments' rows='3' onChange={handleChange} value={session.alphabeticalComments} type='text' className='form-control' />
+          <textarea
+            id="alphabeticalComments"
+            rows="3"
+            onChange={handleChange}
+            value={session.alphabeticalComments}
+            type="text"
+            className="form-control"
+          />
         </div>
       </div>
     </React.Fragment>
