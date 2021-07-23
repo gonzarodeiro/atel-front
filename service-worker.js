2f6d435a08794f85ecf5068a5e13788c36f8f04e
Gonzalo <chalorodeiro@gmail.com>
2021-07-23 17:53:49 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atel-front/precache-manifest.22aef9044b5f67dbe226d3ab751b2afb.js"
+  "/atel-front/precache-manifest.a78cbf6dbd330132ed06f02ba2c35c7f.js"
 );
 
 self.addEventListener('message', (event) => {
