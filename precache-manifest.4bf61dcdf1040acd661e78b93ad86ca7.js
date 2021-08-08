20a7ed3423f88a05fb7d3f7bcbca04e68581d8de
Gonzalo <chalorodeiro@gmail.com>
2021-08-08 14:26:22 -0300

    Updates

--- precache-manifest.4bf61dcdf1040acd661e78b93ad86ca7.js
+++ precache-manifest.4bf61dcdf1040acd661e78b93ad86ca7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b161b312b1c82a75da9e40a8638f3681",
+    "revision": "b82f813e84739a3a94ffba257a361458",
     "url": "/atel-front/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/atel-front/static/css/2.c2774f4f.chunk.css"
   },
   {
-    "revision": "ec701073cccb82e28237",
+    "revision": "4e3d0e019cf26103a79d",
     "url": "/atel-front/static/css/main.a0c7c50f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/atel-front/static/js/2.f6399330.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ec701073cccb82e28237",
-    "url": "/atel-front/static/js/main.4d8c25bd.chunk.js"
+    "revision": "4e3d0e019cf26103a79d",
+    "url": "/atel-front/static/js/main.fadf2c60.chunk.js"
   },
   {
     "revision": "0ed2c031c6f88dda54f0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atel-front/precache-manifest.736b34413b868b4fcfc3a86626a9a17b.js"
+  "/atel-front/precache-manifest.4bf61dcdf1040acd661e78b93ad86ca7.js"
 );
 
 self.addEventListener('message', (event) => {
