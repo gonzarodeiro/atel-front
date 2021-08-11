6d5dadab250ab80c4f774cf7b0da902192baf2c8
Augusto Uricchio <augustouricchio@gmail.com>
2021-08-10 22:45:26 -0300

    add stage

--- src/components/Activity/Logical/index.js
+++ src/components/Activity/Logical/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import { Stage } from 'react-konva';
 
 const Logical = () => {
-  return <div>Lógica</div>;
+  const defaultColor = '#8896AB'; 
+  const CONTAINER_SIZE = '100%';
+  const divRef = useRef(null);
+  const stageRef = useRef(null);
+  
+  const [{ width, height }, setDimensions] = useState({});
+
+  useEffect(() =>{
+    setResolution();
+  },[]);
+
+  function setResolution() {
+    let rect = divRef.current.getBoundingClientRect();
+    const width = rect.width;
+    const height = rect.height;
+    setDimensions({ width, height });  
+  }
+
+  return (
+    <div style={{ width: CONTAINER_SIZE, height: CONTAINER_SIZE,backgroundColor: defaultColor }} ref={divRef}>
+      <Stage width={width} height={height} ref={stageRef}>
+
+      </Stage>
+    </div>
+  )
 };
 
 export default Logical;
