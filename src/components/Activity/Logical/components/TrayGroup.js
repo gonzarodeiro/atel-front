e030772391435c17d4fc1f10399bf61d6f61780f
Augusto Uricchio <augustouricchio@gmail.com>
2021-08-18 16:49:28 -0300

    if tray dont have type dont show it

--- src/components/Activity/Logical/components/TrayGroup.js
+++ src/components/Activity/Logical/components/TrayGroup.js
@@ -20,8 +20,8 @@ const TrayGroup = ({trays, width}) =>{
         <Group type={element.type}>
           <KonvaImage id={"tray-"+index} name={element.id} key={element.id} x={getAllingX(element.width, index, array.length)} y={150} width={element.width} height={element.height} image={imageFactory(element.src)} />
           <Text id={'quantity' + element.id} text={element.quantity} x={getAllingX(element.width, index, array.length) - 20} y={240}  height={20} width={20} fontVariant='bold' fontSize={24} align='center' verticalAlign='middle' strokeWidth={1} fill='white' shadowColor='black' shadowBlur={10} />
-          <Circle x={getAllingX(element.width, index, array.length) + element.width/2} y={240} radius={25} fill="white" stroke="black" />
-          <KonvaImage x={getAllingX(element.width, index, array.length) + element.width/2 -17} y={220} width={30} height={40} image={imageFactory(element.srcType)} />
+          { element.srcType && <Circle x={getAllingX(element.width, index, array.length) + element.width/2} y={240} radius={25} fill="white" stroke="black" />}
+          { element.srcType && <KonvaImage x={getAllingX(element.width, index, array.length) + element.width/2 -17} y={220} width={30} height={40} image={imageFactory(element.srcType)} />}        
           <Text id={'expectedQuantity' + element.id} text={element.expectedQuantity} x={getAllingX(element.width, index, array.length) + element.width} y={240}  height={20} width={20} fontVariant='bold' fontSize={24} align='center' verticalAlign='middle' strokeWidth={1} fill='white' shadowColor='black' shadowBlur={10} />             
         </Group>
         )
