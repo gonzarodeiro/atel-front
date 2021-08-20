617c9c7f1f9d04a1b7773b083e2a07df081301f9
Bruno Sendras <brunosendras@gmail.com>
2021-08-19 22:34:04 -0300

    Add settings modal in numerical activity

--- src/pages/sessions/personal/professional/tools/Numerical.js
+++ src/pages/sessions/personal/professional/tools/Numerical.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBBtn } from 'mdbreact';
 import Jitsi from '../../../../../components/Jitsi';
 import finishSession from '../finishSession';
 import tools from '../../../../../utils/enums/tools';
 import Activity from '../../../../../components/Activity/Logical/professionalLogic';
-
+import Settings, { modalResults } from '../../../../../components/Activity/Logical/components/Settings';
 
 const Numerical = ({ props, handleChange, session, showTools, showMeeting }) => {
+  const [showSettings, setShowSettings] = useState(false);
+
   function redirectTool(tool) {
     showTools({ [tool]: true });
   }
@@ -18,11 +20,22 @@ const Numerical = ({ props, handleChange, session, showTools, showMeeting }) => 
 
   function restart() {}
 
+  function handleOpenSettings() {
+    setShowSettings(true);
+  }
+  function handleCloseSettings(mr, settings) {
+    if (mr === modalResults.OK) {
+      console.log(settings);
+      console.log(settings.containers);
+    }
+    setShowSettings(false);
+  }
+
   return (
     <React.Fragment>
       <div className='row'>
         <div className='pb-3 mt-2 col-md-8'>
-          <Activity/>
+          <Activity />
         </div>
         <div className='col-md-4' style={{ marginTop: '3px' }}>
           <div data-test='col'>
@@ -39,8 +52,13 @@ const Numerical = ({ props, handleChange, session, showTools, showMeeting }) => 
           <div data-test='container' className='container-fluid section mb-3 border p-3 col-md-12'>
             <div className='row'>
               <div className='col-md-12 mt-1 mb-1'>
+                <MDBBtn onClick={handleOpenSettings} size='lg' className='py-2 grey darken-2 shadow-none text-white btnOption w-100 ml-0'>
+                  <span>Configurar</span>
+                </MDBBtn>
+              </div>
+              <div className='col-md-12 mt-1 mb-1'>
                 <MDBBtn onClick={restart} size='lg' className='py-2 green darken-2 shadow-none text-white btnOption w-100 ml-0'>
-                  <span>Reiniciar actividad</span>
+                  <span>Reiniciar</span>
                 </MDBBtn>
               </div>
               <div className='col-md-12 mt-2'>
@@ -75,6 +93,7 @@ const Numerical = ({ props, handleChange, session, showTools, showMeeting }) => 
           <textarea id='numericalComments' rows='3' onChange={handleChange} value={session.numericalComments} type='text' className='form-control' />
         </div>
       </div>
+      <Settings show={showSettings} onClose={handleCloseSettings} />
     </React.Fragment>
   );
 };
