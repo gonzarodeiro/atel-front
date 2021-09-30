ed8932ce1dca8f649cf9ae26c12535ab1167da55
Augusto Uricchio <augustouricchio@gmail.com>
2021-09-30 00:48:14 -0300

    add number sources to tool

---
+++ src/components/Activity/Boxes/commons/elements.js
@@ -0,0 +1,53 @@
+import uno from "../images/numbers/uno.png"
+import dos from "../images/numbers/dos.png"
+import tres from "../images/numbers/tres.png"
+import cuatro from "../images/numbers/cuatro.png"
+import cinco from "../images/numbers/cinco.png"
+import seis from "../images/numbers/seis.png"
+import siete from "../images/numbers/siete.png"
+import ocho from "../images/numbers/ocho.png"
+import nueve from "../images/numbers/nueve.png"
+
+
+
+
+const constElements = [
+  {
+    name : "uno",
+    src : uno
+  },
+  {
+    name : "dos",
+    src : dos
+  },
+  {
+    name : "tres",
+    src : tres
+  },
+  {
+    name : "cuatro",
+    src : cuatro
+  },
+  {
+    name : "cinco",
+    src : cinco
+  },
+  {
+    name : "seis",
+    src : seis
+  },
+  {
+    name : "siete",
+    src : siete
+  },
+  {
+    name : "ocho",
+    src : ocho
+  },
+  {
+    name : "nueve",
+    src : nueve
+  }
+]
+
+export default constElements;
--- src/components/Activity/Boxes/professionalLogic.js
+++ src/components/Activity/Boxes/professionalLogic.js
@@ -1,28 +1,64 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { Stage, Layer} from 'react-konva';
-
+import { Stage, Layer, Image as KonvaImage,} from 'react-konva';
 import Confites from '../../Confites';
-
 import bkgnd from './images/background.jpg';
+import constElements from './commons/elements';
+import { imageFactory } from './commons/imageFactory';
 
 const Boxes = () => {
+
+  const MARGIN = 80;
+  const MARGIN_TOP = 80;
+
   const divRef = useRef(null);
   const stageRef = useRef(null);
   const layerRef = useRef(null);  
   const [{ width, height }, setDimensions] = useState({});    
   const [showConfites, setShowConfites] = useState(false);
+  const [elements, setElements] = useState();
+
 
   useEffect(() => {
+    setConfiguration();
     setDimensions({ width: 700, height: 492 });
   }, []);
 
+  function setConfiguration(){
+     const newElements = getFourRandomElements(constElements);
+     debugger;
+    setElements(newElements);
+  }
 
+  function getFourRandomElements(elements){
+    debugger;
+    let lengthForRandom = elements.length-1;
+    let newElements = [];
+    let randomsGenerated = [];
+    for(let i = 0; i < 4; i++){
+      let number = Math.round(Math.random()*lengthForRandom);
+      if(randomsGenerated.find(x => x == number) === undefined){
+        randomsGenerated.push(number);
+      }else{
+        //deberia hacerlo recursivo, pero paja....
+        let number = Math.round(Math.random()*lengthForRandom);
+        randomsGenerated.push(number);
+      }
+        
+      newElements.push( { name: elements[number].name, src: elements[number].src, charArray: elements[number].name.split("")})
+    }
+    return newElements;
+  }
 
   return (
     <div style={{ width: width, height: height, backgroundSize: 'cover', backgroundImage: `url("${bkgnd}")` }} ref={divRef}>
       <Stage width={width} height={height} ref={stageRef}>
         <Layer ref={layerRef}>
-          
+        {elements &&
+            elements.map((element, index) => (
+              <>                 
+                <KonvaImage  x={MARGIN} y={MARGIN_TOP + (MARGIN + 10) * index} id={'image' + element.name} image={imageFactory(element.src)} height={70} width={50} />                                
+              </>
+            ))}
         </Layer>
         {showConfites && <Confites stageRef={stageRef} />}
       </Stage>      
