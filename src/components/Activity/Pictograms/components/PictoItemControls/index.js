631b796f3ddc5a4bf58656299f33b05e254234f1
Bruno Sendras <brunosendras@gmail.com>
2021-10-01 15:49:30 -0300

    Add missing tooltip in pictogram search results

--- src/components/Activity/Pictograms/components/PictoItemControls/index.js
+++ src/components/Activity/Pictograms/components/PictoItemControls/index.js
@@ -6,7 +6,13 @@ import './styles.css';
 const PictoItemControls = ({ className, arrowsVisible, plusVisible, onClickLeft, onClickRight, onClickAdd }) => (
   <div className={`pic-picto-item-controls ${className}`}>
     {arrowsVisible && <i className='fas fa-chevron-left pic-picto-btn-up' onClick={onClickLeft} />}
-    {plusVisible && <i className='fas fa-plus pic-picto-btn-up' onClick={onClickAdd} />}
+    {plusVisible && (
+      <OverlayTrigger trigger='hover' overlay={<Tooltip id='tooltip-disabled'>Agregar a plantilla del alumno</Tooltip>} placement='left'>
+        <div>
+          <i className='fas fa-plus pic-picto-btn-up' onClick={onClickAdd} />
+        </div>
+      </OverlayTrigger>
+    )}
     {arrowsVisible && <i className='fas fa-chevron-right pic-picto-btn-down' onClick={onClickRight} />}
   </div>
 );
