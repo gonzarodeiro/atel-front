27c27c9d19facb0401de30ff7994e4cbc78c02fd
Bruno Sendras <brunosendras@gmail.com>
2021-10-10 13:57:08 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atel-front/precache-manifest.3902e77336c2afba27b2792310a94da5.js"
+  "/atel-front/precache-manifest.66c54432ab8ecc6f81b2e8d849282043.js"
 );
 
 self.addEventListener('message', (event) => {
