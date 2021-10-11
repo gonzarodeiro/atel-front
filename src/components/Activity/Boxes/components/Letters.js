c36b46604387edbb681e96746078606f4a8b263e
Augusto Uricchio <augustouricchio@gmail.com>
2021-10-11 18:27:23 -0300

    fix boxes

--- src/components/Activity/Boxes/components/Letters.js
+++ src/components/Activity/Boxes/components/Letters.js
@@ -92,6 +92,7 @@ const Letters = ({ element, indexElement, letters, setCorrectElement, stageRef }
 
     let interval = window.setInterval(function () {
       document.addEventListener('click', (event) => {
+        document.removeEventListener('keydown', inputLetter);
         window.clearInterval(interval);
         target.clearCache();
         target.filters([]);
