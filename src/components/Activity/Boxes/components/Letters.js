f902684deef2515501750526cdf93195f3df36a3
Augusto Uricchio <augustouricchio@gmail.com>
2021-10-15 00:57:27 -0300

    fix

--- src/components/Activity/Boxes/components/Letters.js
+++ src/components/Activity/Boxes/components/Letters.js
@@ -7,6 +7,7 @@ import emptyLetter from '../images/emptyLetter.png';
 import Konva from 'konva';
 import { clientEvents, sendMessage } from '../../../../utils/socketManager';
 
+
 const Letters = ({ element, indexElement, letters, setCorrectElement, stageRef }) => {
   const MARGIN = 80,
     MARGIN_TOP = 80;
@@ -48,8 +49,8 @@ const Letters = ({ element, indexElement, letters, setCorrectElement, stageRef }
     );
   };
 
-  const handleOnClick = (e, i) => {
-    sendMessage(clientEvents.clickLetter + indexElement, i)
+  const handleOnClick = (e, i) => {    
+    sendMessage(clientEvents.clickLetter + indexElement, i)    
     let target = e.currentTarget.children[0];
     let greenIncrement = 5;
 
@@ -94,14 +95,17 @@ const Letters = ({ element, indexElement, letters, setCorrectElement, stageRef }
     }
     document.addEventListener('keydown', inputLetter);
 
-    let interval = window.setInterval(function () {
-      document.addEventListener('click', (event) => {
-        sendMessage(clientEvents.clearIntervals);
+    const clickEvent = (event) => {      
+        //sendMessage(clientEvents.clearIntervals);
         document.removeEventListener('keydown', inputLetter);
         window.clearInterval(interval);
         target.clearCache();
         target.filters([]);
-      });
+        document.removeEventListener('click',clickEvent);        
+    }
+
+    let interval = window.setInterval(function () {
+      document.addEventListener('click',clickEvent);
       target.cache();
       target.filters([Konva.Filters.RGB]);
       target.blue(255);
@@ -117,8 +121,8 @@ const Letters = ({ element, indexElement, letters, setCorrectElement, stageRef }
           return (
             <Group id={'group' + element.name + index} onClick={(e) => handleOnClick(e, index)} onMouseOut={() => handleOnMouseOut()} onMouseOver={() => handleOnMouseOver(index)}>
               <KonvaImage scaleX={letter.isOnMouseUp ? 1.2 : 1} scaleY={letter.isOnMouseUp ? 1.2 : 1} x={MARGIN + 50 + 60 * index} y={MARGIN_TOP + (MARGIN + 10) * indexElement} id={'letterImage' + element.name + letter} image={imageFactory(letter.src)} height={70} width={50} />
-              <Text id={'letterImage' + element.name + letter} text={letter.letterInput} x={MARGIN + 55 + 60 * index} y={MARGIN_TOP + 10 + (MARGIN + 10) * indexElement} fontVariant='bold' fontSize={42} align='center' verticalAlign='middle' strokeWidth={1} fill='black' shadowColor='white' shadowBlur={10} />
-            </Group>
+              <Text id={'letterImage' + element.name + letter} text={letter.letterInput} x={MARGIN + 55 + 60 * index} y={MARGIN_TOP + 10 + (MARGIN + 10) * indexElement} fontVariant='bold' fontSize={42} align='center' verticalAlign='middle' strokeWidth={1} fill='black' shadowColor='white' shadowBlur={10} />              
+            </Group>          
           );
         })}
     </>
