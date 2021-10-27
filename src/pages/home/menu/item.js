007d9fd7ab1b2c44f69cb46d6d2511aaae9ef78d
Gonzalo <chalorodeiro@gmail.com>
2021-10-27 16:20:04 -0300

    Adding items in home

--- src/pages/home/menu/item.js
+++ src/pages/home/menu/item.js
@@ -18,13 +18,13 @@ const Item = ({ title, colorStyle, firstItem, secondItem, redirectFirstItem, red
       <div data-test='col' className='col-md-12 col-lg-4 mb-lg-0 mb-4'>
         <div data-test='card' className='card ' style={{ borderRadius: '20px' }}>
           <div data-test='card-body' className='card-body animated zoomIn faster'>
-            <h5 className='text-center' style={{ color: [colorStyle], fontSize: '18.2px', borderBottom: borderBottom, padding: '0px 0px 4px 0px', marginBottom: '14px' }}>
+            <h5 className='text-center' style={{ color: [colorStyle], fontSize: '20px', fontWeight: '600', borderBottom: borderBottom, padding: '0px 0px 4px 0px', marginBottom: '14px' }}>
               {title}
             </h5>
-            <label onClick={() => redirect(redirectFirstItem)} className='dark-grey-text' style={{ cursor: 'pointer' }}>
+            <label onClick={() => redirect(redirectFirstItem)} className='dark-grey-text' style={{ cursor: 'pointer', marginTop: '16px', marginBottom: '16px' }}>
               - {firstItem}
             </label>
-            <label onClick={() => redirect(redirectSecondItem)} className='dark-grey-text mb-0' style={{ cursor: 'pointer' }}>
+            <label onClick={() => redirect(redirectSecondItem)} className='dark-grey-text' style={{ cursor: 'pointer', marginBottom: '54px' }}>
               - {secondItem}
             </label>
           </div>
