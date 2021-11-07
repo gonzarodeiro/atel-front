f2b2d4066c63c57b86ca1c439a0e2199f7204b83
Bruno Sendras <brunosendras@gmail.com>
2021-11-06 21:11:45 -0300

    Display pictogram selection layout in columns

--- src/components/Activity/Pictograms/components/PictoList/index.js
+++ src/components/Activity/Pictograms/components/PictoList/index.js
@@ -8,7 +8,7 @@ function getBadgeCount(pictos, loading) {
   return (pictos && pictos.length) || 0;
 }
 const PictoList = ({ className, pictos, onItemClick, onItemClickAdd, onItemClickRemove, loading, labelText, placeholderText, addItemVisible, removeItemVisible }) => (
-  <div id='drawer' className={`${className}`}>
+  <div className={`pic-drawer-container ${className}`}>
     {/* boton para abrir la lista de seleccion manual */}
     <div className='pic-drawer-grip-container'>
       <p className='pic-drawer-label'>{labelText}</p>
@@ -16,7 +16,7 @@ const PictoList = ({ className, pictos, onItemClick, onItemClickAdd, onItemClick
       <i className='fas fa-chevron-right' style={{ color: 'grey' }}></i>
     </div>
     {/* lista de seleccion manual */}
-    <PictoBasicList id='list' pictos={pictos} onItemClick={onItemClick} onItemClickAdd={onItemClickAdd} onItemClickRemove={onItemClickRemove} addItemVisible={addItemVisible} removeItemVisible={removeItemVisible} placeholderText={placeholderText} loading={loading} />
+    <PictoBasicList className='pic-basic-list fix-height' pictos={pictos} onItemClick={onItemClick} onItemClickAdd={onItemClickAdd} onItemClickRemove={onItemClickRemove} addItemVisible={addItemVisible} removeItemVisible={removeItemVisible} placeholderText={placeholderText} loading={loading} />
   </div>
 );
 
