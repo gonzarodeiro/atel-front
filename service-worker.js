b6780c3369f87e66b3663ac722cb99548d6f9fc8
Gonzalo <chalorodeiro@gmail.com>
2021-11-27 14:51:21 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6161048d859725e3a9dea8ad7e3fe6da.js"
+  "./precache-manifest.3b5cec58d54522b64e5ac7e3faab9e54.js"
 );
 
 self.addEventListener('message', (event) => {
